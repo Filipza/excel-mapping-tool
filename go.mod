6a308d5ca5792647e3fda65999ffcdd9307817ab
Filip Zawartka <filip.zawartka@skon.de>
2024-03-12 13:40:15 +0100

    implement first unit tests

--- go.mod
+++ go.mod
@@ -2,9 +2,17 @@ module github.com/Filipza/excel-mapping-tool
 
 go 1.22.0
 
-require github.com/xuri/excelize/v2 v2.8.1
+require (
+	github.com/stretchr/testify v1.9.0
+	github.com/xuri/excelize/v2 v2.8.1
+)
 
-require golang.org/x/sys v0.17.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
 
 require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
--- internal/domain/dataimport/service.go
+++ internal/domain/dataimport/service.go
@@ -1,6 +1,85 @@
 package dataimport
 
+import (
+	"errors"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/xuri/excelize/v2"
+)
+
 type MappingService interface {
 	ReadFile(UploadData) (MappingOptions, error)
 	WriteMapping(MappingInstruction) (MappingResult, error)
+}
+
+type mappingService struct {
+}
+
+func (svc *mappingService) ReadFile(ud *UploadData) (*MappingOptions, error) {
+	xlsx, err := excelize.OpenReader(ud.UploadedFile)
+	if err != nil {
+		log.Debug(err)
+		return nil, &Error{
+			ErrTitle: "Parsingfehler",
+			ErrMsg:   "Datei konnte nicht verarbeitet werden und möglicherweise korrupt.",
+		}
+	}
+
+	ddOptions := make(map[string]string)
+	ddOptions["wkz"] = "Webekostenzuschuss"
+	ddOptions["stocks"] = "Lagerbestand"
+
+	mappingOptions := MappingOptions{
+		DropdownOptions: ddOptions,
+		TableSummary:    make([][]string, 0),
+	}
+
+	sheetLists := xlsx.GetSheetList()
+	if len(sheetLists) == 0 {
+		log.Debug(err)
+		return nil, &Error{
+			ErrTitle: "Fehlerhafte Excel-Datei",
+			ErrMsg:   "Datei enthält keine Arbeitsblätter",
+		}
+	}
+
+	rows, err := xlsx.Rows(sheetLists[0])
+	if err != nil {
+		log.Debug(err)
+		return nil, &Error{
+			ErrTitle: "Parsingfehler",
+			ErrMsg:   "Es is ein Fehler beim Lesen der Reihen aufgetreten. Überprüfe die Datei.",
+		}
+	}
+
+	for i := 0; i < 4 && rows.Next(); i++ {
+		col, err := rows.Columns()
+
+		if err != nil {
+			log.Debug(err)
+			return nil, &Error{
+				ErrTitle: "Parsingfehler",
+				ErrMsg:   "Es is ein Fehler beim Lesen der Reihen aufgetreten. Überprüfe die Datei.",
+			}
+		}
+
+		if len(col) == 0 {
+			log.Debug(errors.New("first row is empty"))
+			return nil, &Error{
+				ErrTitle: "Dateifehler",
+				ErrMsg:   "Die erste Zeile der Datei ist leer. Diese muss für den Import die Tabellenköpfe enthalten.",
+			}
+		}
+
+		if i == 0 {
+			mappingOptions.TableHeaders = col
+			continue
+		}
+
+		mappingOptions.TableSummary = append(mappingOptions.TableSummary, col)
+
+	}
+	fmt.Println(mappingOptions)
+	return &mappingOptions, nil
 }
